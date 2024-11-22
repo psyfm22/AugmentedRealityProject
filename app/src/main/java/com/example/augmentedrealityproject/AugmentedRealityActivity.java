1d385d399304cd8aed3a1458fe56ce1698070884
Freddie Moir <freddiemoir@icloud.com>
2024-11-22 13:01:50 +0000

    Now checks if AR core is supported on the device

--- app/src/main/java/com/example/augmentedrealityproject/AugmentedRealityActivity.java
+++ app/src/main/java/com/example/augmentedrealityproject/AugmentedRealityActivity.java
@@ -3,8 +3,13 @@ package com.example.augmentedrealityproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.ar.core.ArCoreApk;
+
 
 public class AugmentedRealityActivity extends AppCompatActivity {
 
@@ -17,5 +22,22 @@ public class AugmentedRealityActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String location = intent.getStringExtra("LOCATION");
         Log.d("COMP3018", "Here is location: "+ location);
+
+        maybeEnableArButton();
+
+    }
+
+    void maybeEnableArButton() {
+        Button mArButton = findViewById(R.id.mArButton);
+        ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
+            if (availability.isSupported()) {
+                Log.d("COMP3018", "AR is supported");
+                mArButton.setVisibility(View.VISIBLE);
+                mArButton.setEnabled(true);
+            } else { // The device is unsupported or unknown.
+                Log.d("COMP3018", "AR is not supported");
+                mArButton.setVisibility(View.INVISIBLE);
+                mArButton.setEnabled(false);
+            }
     }
 }
